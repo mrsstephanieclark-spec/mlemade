6d473be599bc67d8a9fc291898579e18f2a46832
mrsstephanieclark-spec <mrsstephanieclark-spec@users.noreply.github.com>
2026-08-20 16:12:11 -0400

    ✨ Add Facebook social icon link and update Instagram link in the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -19,6 +19,14 @@ function TikTokIcon() {
   );
 }
 
+function FacebookIcon() {
+  return (
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M18 2h-3a5 5 0 0 0 -5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+    </svg>
+  );
+}
+
 const footerLinks = [
   { label: 'Home', href: '#home' },
   { label: 'Services', href: '#services' },
@@ -31,8 +39,13 @@ const footerLinks = [
 const socialLinks = [
   {
     label: 'Instagram',
-    href: 'https://instagram.com/mle_made',
+    href: 'https://www.instagram.com/mle_made/',
     icon: InstagramIcon,
+  },
+  {
+    label: 'Facebook',
+    href: 'https://www.facebook.com/MLEmade/',
+    icon: FacebookIcon,
   },
   {
     label: 'TikTok',
